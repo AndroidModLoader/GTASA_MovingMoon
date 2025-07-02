9ee0cb3b3f22406af74bdcdca976b471be05bbf8
RusJJ <mctesting1@yandex.ru>
2025-07-02 13:01:39 +0300

    Not yet moving moon with phases

--- main.cpp
+++ main.cpp
@@ -6,12 +6,14 @@
 #include <math.h>
 #include <time.h>
 
+#define MOON_DRAW_DIST          (150.0f)
 #define IMPROVED_MOON_HEIGHT    (50.0f)
 
 #include "SimpleGTA.h"
 #include "vars.inl"
+#include "iskygfx.h"
 
-MYMOD(net.rusjj.movingmoon, Moving Moon, 1.0.1, CowBoy69 & RusJJ)
+MYMOD(net.rusjj.movingmoon, Moving Moon, 1.1, CowBoy69 & RusJJ)
 NEEDGAME(com.rockstargames.gtasa)
 
 uintptr_t pGTASA;
@@ -19,6 +21,7 @@ void *hGTASA;
 
 CVector MoonVector;
 bool MoonVisible = false;
+ISkyGFX* skygfx = NULL;
 
 
 
@@ -67,26 +70,68 @@ extern "C" void RenderMoon_Patch()
         if (moonfadeout > 0 && moonfadeout < 340)
         {
             CVector& vecsun = m_VectorToSun[*m_CurrentStoredValue];
-            MoonVector = { -vecsun.x, -vecsun.y, -(IMPROVED_MOON_HEIGHT / 150.0f) * vecsun.z }; // normalized vector (important for DotProd)
-            RwV3d pos = { 150.0f * MoonVector.x, 150.0f * MoonVector.y, 150.0f * MoonVector.z };
+            MoonVector = { -vecsun.x, -vecsun.y, -(IMPROVED_MOON_HEIGHT / MOON_DRAW_DIST) * vecsun.z }; // normalized vector (important for DotProd)
+            RwV3d pos = {
+                MOON_DRAW_DIST * MoonVector.x,
+                MOON_DRAW_DIST * MoonVector.y,
+                MOON_DRAW_DIST * MoonVector.z
+            };
 
             CamPos = &(*(CMatrix**)(TheCamera + BYBIT(20, 24)))->pos;
             
             worldpos = pos + *CamPos;
             if(CalcScreenCoors(&worldpos, &screenpos, &szx, &szy, false, true))
             {
+                screenpos.z = *ms_fFarClipZ;
+                
                 MoonVisible = true;
+                float sz = *MoonSize * 2.7f + 4.0f;
+                int brightness = decoverage * moonfadeout;
 
                 RwRenderStateSet(8, (void*)0);
                 RwRenderStateSet(6, (void*)0);
                 RwRenderStateSet(12, (void*)1);
-                RwRenderStateSet(10, (void*)2);
-                RwRenderStateSet(11, (void*)2);
-        
-                RwRenderStateSet(1, *(gpCoronaTexture[2]));
-                float sz = *MoonSize * 2.7f + 4.0f;
-                int brightness = decoverage * moonfadeout;
-                RenderBufferedOneXLUSprite(screenpos, szx * sz, szy * sz, brightness, brightness, brightness, 255, 1.0f / screenpos.z, 255);
+
+                if(skygfx && skygfx->GetFeatureLevel(GFX_MOONPHASES))
+                {
+                    szx *= sz;
+                    szy *= sz;
+
+                    float moonPhaseStep = (*ms_nGameClockDays / 31) - 0.5f;
+
+                    // Base
+                    RwRenderStateSet(1, NULL);
+                    RwRenderStateSet(10, (void*)5);
+                    RwRenderStateSet(11, (void*)2);
+                    RenderBufferedOneXLUSprite(screenpos, szx, szy, 0, 0, 0, 255, 1.0f / screenpos.z, 255);
+
+                    // Moon mask
+                    CVector maskScreenpos = screenpos;
+                    maskScreenpos.x += moonPhaseStep * 2.0f * (2.7f * szx);
+                    maskScreenpos.y += 0.7f * szy;
+                    RwRenderStateSet(1, *gpMoonMask);
+                    RwRenderStateSet(10, (void*)5);
+                    RwRenderStateSet(11, (void*)2);
+                    skygfx->PreMoonMaskRender();
+                    RenderBufferedOneXLUSprite(maskScreenpos, 1.7f * szx, 1.7f * szy, 0, 0, 0, 0, 1.0f / screenpos.z, 255);
+                    skygfx->PostMoonMaskRender();
+            
+                    // Actual moon
+                    RwRenderStateSet(1, *(gpCoronaTexture[2]));
+                    RwRenderStateSet(10, (void*)7);
+                    RwRenderStateSet(11, (void*)2);
+                    RwRenderStateSet(8, (void*)0);
+                    RenderBufferedOneXLUSprite(screenpos, szx, szy, brightness, brightness, 0.85f * brightness, 255, 1.0f / screenpos.z, 255);
+                }
+                else
+                {
+                    RwRenderStateSet(1, *(gpCoronaTexture[2]));
+                    RwRenderStateSet(10, (void*)5);
+                    RwRenderStateSet(11, (void*)2);
+                    RwRenderStateSet(8, (void*)0);
+                    RenderBufferedOneXLUSprite(screenpos, szx * sz, szy * sz, brightness, brightness, 0.85f * brightness, 255, 1.0f / screenpos.z, 255);
+                }
+                
                 FlushSpriteBuffer();
             }
         }
@@ -118,7 +163,7 @@ __attribute__((optnone)) __attribute__((naked)) void RenderMoon_Inject(void)
 #endif
 }
 
-extern "C" void OnModLoad()
+extern "C" void OnAllModsLoaded()
 {
     logger->SetTag("MovingMoon");
     pGTASA = aml->GetLib("libGTASA.so");
@@ -133,9 +178,12 @@ extern "C" void OnModLoad()
     
     SET_TO(Foggyness, aml->GetSym(hGTASA, "_ZN8CWeather9FoggynessE"));
     SET_TO(CloudCoverage, aml->GetSym(hGTASA, "_ZN8CWeather13CloudCoverageE"));
+    SET_TO(ms_fFarClipZ, aml->GetSym(hGTASA, "_ZN5CDraw12ms_fFarClipZE"));
     SET_TO(gpCoronaTexture, aml->GetSym(hGTASA, "gpCoronaTexture"));
+    SET_TO(gpMoonMask, aml->GetSym(hGTASA, "gpMoonMask"));
     SET_TO(ms_nGameClockHours, aml->GetSym(hGTASA, "_ZN6CClock18ms_nGameClockHoursE"));
     SET_TO(ms_nGameClockMinutes, aml->GetSym(hGTASA, "_ZN6CClock20ms_nGameClockMinutesE"));
+    SET_TO(ms_nGameClockDays, aml->GetSym(hGTASA, "_ZN6CClock17ms_nGameClockDaysE"));
     SET_TO(m_CurrentStoredValue, aml->GetSym(hGTASA, "_ZN10CTimeCycle20m_CurrentStoredValueE"));
     SET_TO(MoonSize, aml->GetSym(hGTASA, "_ZN8CCoronas8MoonSizeE"));
     SET_TO(TheCamera, aml->GetSym(hGTASA, "TheCamera"));
@@ -151,4 +199,6 @@ extern "C" void OnModLoad()
 
     RenderMoon_BackTo = pGTASA + BYBIT(0x59EEE2 + 0x1, 0x6C2E98);
     aml->Redirect(pGTASA + BYBIT(0x59EC30 + 0x1, 0x6C2BD4), (uintptr_t)RenderMoon_Inject);
+
+    skygfx = (ISkyGFX*)GetInterface("SkyGFX");
 }
--- vars.inl
+++ vars.inl
@@ -3,9 +3,9 @@ void (*FlushSpriteBuffer)();
 bool (*CalcScreenCoors)(CVector*, CVector*, float*, float*, bool, bool);
 void (*RenderBufferedOneXLUSprite)(CVector, float, float, uint8_t, uint8_t, uint8_t, short, float, uint8_t);
 
-float *Foggyness, *CloudCoverage;
-void ***gpCoronaTexture;
-uint8_t *ms_nGameClockHours, *ms_nGameClockMinutes;
+float *Foggyness, *CloudCoverage, *ms_fFarClipZ;
+void ***gpCoronaTexture, **gpMoonMask;
+uint8_t *ms_nGameClockHours, *ms_nGameClockMinutes, *ms_nGameClockDays;
 int32_t *m_CurrentStoredValue;
 uint32_t *MoonSize;
 char* TheCamera;
